714ca3571ea8ec220f9d9849a00d9e752fff3def
fbmnds <fbmnds7@gmail.com>
2013-09-01 12:54:21 +0200

    added first part of functional tests

--- test/matrixtests_test.clj
+++ test/matrixtests_test.clj
@@ -4,7 +4,8 @@
             [criterium.core :as crit]
             [clatrix.core :as clx]
             [clojure.core.matrix :as M]
-            [clojure.string :as str])
+            [clojure.string :as str]
+            [clojure.inspector :as insp])
   (:use midje.sweet)
   (:import [org.jblas DoubleMatrix]))
 
@@ -57,17 +58,6 @@
      (println "msecs : " (msecs ~expr))))
 
 
-(def nx 100)
-(def ny 100)
-(def nx-1 (dec nx))
-(def ny-1 (dec ny))
-(def nx-2 (dec nx-1))
-(def ny-2 (dec ny-1))
-(def dd (make-array Double/TYPE nx ny))
-(def DD (clx/matrix (DoubleMatrix. ^"[[D" dd)))
-(def DCM (M/matrix dd))
-
-
 (defmacro bench-nx-ny [expr]
   `(crit/bench (dotimes [~'i nx] (dotimes [~'j ny] ~expr))))
 
@@ -79,6 +69,12 @@
 
 (defmacro once-nx [expr]
   `(measure (dotimes [~'i nx] ~expr)))
+
+(defmacro bench-x-y [x y expr]
+  `(crit/bench (dotimes [~'i ~x] (dotimes [~'j ~x] ~expr))))
+
+(defmacro once-x-y [x y expr]
+  `(measure (dotimes [~'i ~x] (dotimes [~'j ~x] ~expr))))
 
 (defmacro bench-x [x expr]
   `(crit/bench (dotimes [~'i ~x] ~expr)))
@@ -95,10 +91,134 @@
 
 (def start (. System (nanoTime)))
 
-(fact "test" :test
-      (once-x 2 (println (let [#^doubles a (aget #^objects dd i)] (aget a 0))))
-      => nil)
+(underline "inititalizing the tests:")
 
+(def nx 100)
+(def ny 100)
+(def nx-1 (dec nx))
+(def ny-1 (dec ny))
+(def nx-2 (dec nx-1))
+(def ny-2 (dec ny-1))
+(def dd (make-array Double/TYPE nx ny))
+(once-nx-ny (cg-aset! dd i j 42.0))
+(def DD (clx/matrix (DoubleMatrix. ^"[[D" dd)))
+(def DCM (M/matrix dd))
+
+(def CNULL  (clx/matrix [[0.0 0.0] [0.0 0.0]]))
+(def CMNULL (M/new-matrix 2 2))
+
+(def T   [[42.0 42.0] [42.0 42.0]])
+(def T*2 [[84.0 84.0] [84.0 84.0]])
+(def T*T [[3528.0 3528.0] [3528.0 3528.0]])
+
+(def CT   (clx/matrix T))
+(def CT*2 (clx/matrix T*2))
+(def CT*T (clx/matrix T*T))
+
+(def CMT   (M/matrix T))
+(def CMT*2 (M/matrix T*2))
+(def CMT*T (M/matrix T*T))
+
+(fact-group
+ :ftests
+ (underline "functional tests")
+
+ (fact "optimal hints on aget, CGrande"
+       [(let [#^doubles a (aget #^objects dd 0)] (aget a 0))
+        (let [#^doubles a (aget #^objects dd (dec nx))] (aget a (dec ny)))]
+       => [42.0 42.0])
+
+ (fact "optimal hints/macros on aget, CGrande"
+       [(deep-aget doubles dd 0 0)
+        (deep-aget doubles dd (dec nx) (dec ny))]
+       => [42.0 42.0])
+
+ (println "... omitting further get/set tests\n")
+
+
+ (fact "clx/+ is ok for Clatrix matrix"
+       (clx/+ CT CT) => CT*2)
+
+ (try
+   (do
+     (println)
+     (underline "clx/+ is not suitable for clojure array/matrix")
+     (clx/+ T T))
+   (catch Exception e (println (.getMessage e))))
+
+ (try
+   (do
+     (println)
+     (underline "clx/+ is not suitable for core.matrix matrix")
+     (clx/+ CMT CMT))
+   (catch Exception e (println (.getMessage e))))
+ (println)
+
+ (underline "core.matrix provides convenient handling of different matrix types")
+ (fact "M/add is ok for core.matrix matrix"
+       (M/add CMT CMT) => CMT*2)
+
+ (fact "M/add is ok for clojure array/matrix"
+       (M/add T T) => CMT*2)
+
+ (fact "M/add is ok for Clatrix matrix"
+       (M/add CT CT) => CMT*2)
+
+ (println)
+ (underline "testing matrix subtraction")
+ (fact "clx/- is ok for Clatrix matrix"
+       (clx/- CT CT) => CNULL)
+
+ (fact "M/sub is ok for core.matrix matrix"
+       (M/sub CMT CMT) => CMNULL)
+
+ (fact "M/sub is ok for clojure array/matrix"
+       (M/sub T T) => CMNULL)
+
+ (fact "M/sub is ok for Clatrix matrix"
+       (M/sub CT CT) => CMNULL)
+
+ (println)
+ (underline "testing matrix multiplication (inner product)")
+ (fact "clx/* is ok for Clatrix matrix"
+       (clx/* CT CT) => CT*T)
+
+ (println)
+ (underline "M/mul is documented to be elementwise product, but provides inner product")
+ (println "ref. https://github.com/mikera/matrix-api/blob/master/src/main/clojure/clojure/core/matrix.clj#L609")
+ (fact "M/mul is 'ok' for core.matrix matrix"
+       (M/mul CMT CMT) => CMT*T)
+
+ (fact "M/mul is 'ok' for clojure array/matrix"
+       (M/mul T T) => CMT*T)
+
+ (fact "M/mul is 'ok' for Clatrix matrix"
+       (M/mul CT CT) => CMT*T)
+
+ (println)
+ (underline "core.matrix/mmul should be inner product, but throws compiler exception")
+ (println "ref. https://github.com/mikera/matrix-api/blob/master/src/main/clojure/clojure/core/matrix.clj#L630")
+ (println "CompilerException java.lang.RuntimeException:")
+ (println "No such var: M/mmul, compiling:(matrixtests_test.clj:...:1)")
+
+ (println)
+ (underline "testing matrix multiplication (scalar product)")
+ (fact "clx/* is ok for Clatrix matrix"
+       (clx/* 2. CT) => CT*2)
+
+ (fact "M/mul is ok for core.matrix matrix"
+       (M/mul 2. CMT) => CMT*2)
+
+ (fact "M/mul is ok for clojure array/matrix"
+       (M/mul 2. T) => CMT*2)
+
+ (fact "M/mul is ok for Clatrix matrix"
+       (M/mul 2. CT) => CMT*2)
+
+ (println)
+ (underline "testing submatrices")
+
+ )
 
 (fact "aget! variants" :aget
 
@@ -123,7 +243,7 @@
       (once-nx-ny  (f-cg-aget! dd i j))
       (bench-nx-ny (f-cg-aget! dd i j))
 
-      (header "optimal hints/macros/cg-aget! on aget, LJensen")
+      (header "optimal hints/macros/lj-aget! on aget, LJensen")
       (once-nx-ny  (lj-aget! dd i j))
       (bench-nx-ny (lj-aget! dd i j))
 
@@ -160,7 +280,7 @@
       (once-nx-ny  (f-cg-aset! dd i j 42.0))
       (bench-nx-ny (f-cg-aset! dd i j 42.0))
 
-      (header "optimal hints/macros/jl-aset! on aset, LJensen")
+      (header "optimal hints/macros/lj-aset! on aset, LJensen")
       (once-nx-ny  (lj-aset! dd i j 42.0))
       (bench-nx-ny (lj-aset! dd i j 42.0))
 
@@ -243,11 +363,6 @@
       (header "use core.matrix/sub")
       (once (M/sub (E-3 DD nx-2 ny-2) (E-3 DD nx-2 ny-2)))
       (bench (M/sub (E-3 DD nx-2 ny-2) (E-3 DD nx-2 ny-2)))
-
-      (header "use core.matrix/mmul throws compiler Exception")
-      (println "CompilerException java.lang.RuntimeException:")
-      (println "No such var: M/mmul, compiling:(matrixtests_test.clj:...:1)")
-      (println "ref. https://github.com/mikera/matrix-api/blob/master/src/main/clojure/clojure/core/matrix.clj#L630")
 
       true => truthy)
 
